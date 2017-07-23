50838b1279a3c159bcba1c0a2f6ed226e437dfd1
Sofia <sofia@sofia.local>
2017-07-23 15:37:42 -0400

    added weather api and search

---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { API_KEY } from '../secrets/constant';
+
+// const API_KEY = '7bb466b11ea405c518b3af4bfff363b5';
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+// middlewares are functions that take an action and depending on the action type or payload, the middleware can choose for the action to pass through, stop it, manipulate it, console log it etc before it reaches any reducer (gate keepers)
+
+// why would you want to change the action?
+
+// reduce promise is a famous middleware package
+// To keep the action types more consistents, saving it in a constant
+// if for some reason someone changed it, it will break the code.
+// saving it in a variable makes it safer (convention)
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+
+
+// axios solely for ajax calls
+//payload that contains additional data (optional)
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,41 @@
 import React, {Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  // initializing the state for the component
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+
+    // bind the function to this (which is search bar), and replace on change, with the new instance of the function
+    // necessary if the callback function references 'this'
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+  onFormSubmit(event){
+    event.preventDefault();
+    // we need to go fetch the weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+
+
+  }
+
   render() {
     return(
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favourite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
@@ -12,3 +43,13 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+// getting this from the reducer
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
+
+// null is here because whenever we are passing in a function, it always goes in as the second argument
+// no state necessary
