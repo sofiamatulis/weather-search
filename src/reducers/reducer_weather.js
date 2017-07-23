f468e515993ee12668fe8b039e7f13f63ce1e481
Sofia <sofia@sofia.local>
2017-07-23 16:16:12 -0400

    rendering city name

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index'
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   switch(action.type) {
     case FETCH_WEATHER:
     return [action.payload.data, ...state]; // [city, city, city]
